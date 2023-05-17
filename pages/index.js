d2dc2ff0d9f62709889c08c9d64ae1d2c1b6bd5a
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-05-17 14:26:39 -0400

    Feature: Designed logos and added modern layout.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from "react";
 import * as mobilenet from "@tensorflow-models/mobilenet";
-import * as tf from '@tensorflow/tfjs';
-import axios from 'axios';
+import * as tf from "@tensorflow/tfjs";
+import axios from "axios";
+import Image from "next/image";
 
 export default function Home() {
   const [isModelLoading, setIsModelLoading] = useState(false);
@@ -31,25 +32,25 @@ export default function Home() {
     const { files } = e.target;
     if (files.length > 0) {
       const formData = new FormData();
-      formData.append('image', files[0]);
+      formData.append("image", files[0]);
 
       try {
-        const response = await axios.post('/api/upload', formData, {
+        const response = await axios.post("/api/upload", formData, {
           headers: {
-            'Content-Type': 'multipart/form-data',
+            "Content-Type": "multipart/form-data",
           },
         });
 
         // response.data.imageUrl is the image URL returned from the server
         setImageURL(response.data.imageUrl);
       } catch (error) {
-        console.error('Error uploading image: ', error);
+        console.error("Error uploading image: ", error);
       }
     }
   };
 
   const identify = async () => {
-    textInputRef.current.value = '';
+    textInputRef.current.value = "";
     const results = await model.classify(imageRef.current);
     setResults(results);
   };
@@ -79,61 +80,98 @@ export default function Home() {
 
   return (
     <div className="container py-5">
-      <h1 className="header text-center mb-5">ImageSense</h1>
-      <h2 className="text-center mb-5">Image Identification</h2>
-      <div className="row mb-3">
-        <div className="col">
-          <input type="file" accept="image/*" capture="camera" className="form-control" onChange={uploadImage} ref={fileInputRef} />
-        </div>
-        <div className="col">
-          <button className="btn btn-primary" onClick={triggerUpload}>Upload Image</button>
-        </div>
-        <div className="col">
-          <input type="text" className="form-control" placeholder="Paste image URL" ref={textInputRef} onChange={handleOnChange} />
-        </div>
-      </div>
-      <div className="row text-center">
+      <h1 className="text-center visually-hidden">ImageSense</h1>
+
+      <div className="row justify-content-center">
         <div className="col-md-6">
-          <div className="imageHolder">
-            {imageURL && <img src={imageURL} alt="Upload Preview" crossOrigin="anonymous" ref={imageRef} className="img-fluid rounded shadow mb-3" />}
+          <div className="text-center mb-3">
+            <Image
+              src="/ImageSense-logos/ImageSense-logos_black.png"
+              alt="ImageSense Logo"
+              className="img-fluid"
+              width={500}
+              height={300}
+            />
           </div>
-          {results.length > 0 && (
+          <div className="text-center">
+            <input
+              type="file"
+              accept="image/*"
+              capture="camera"
+              className="form-control"
+              onChange={uploadImage}
+              ref={fileInputRef}
+            />
+          </div>
+          <div className="text-center">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Paste image URL"
+              ref={textInputRef}
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="text-center mt-3">
+            {imageURL && (
+              <button className="btn btn-success" onClick={identify}>
+                Identify Image
+              </button>
+            )}
+          </div>
+          <div className="text-center mt-4">
+            <div className="imageHolder">
+              {imageURL && (
+                <img
+                  src={imageURL}
+                  alt="Upload Preview"
+                  crossOrigin="anonymous"
+                  ref={imageRef}
+                  className="img-fluid rounded shadow mb-3"
+                />
+              )}
+            </div>
             <div className="resultsHolder">
-              {results.map((result, index) => {
-                return (
-                  <div className="alert alert-success" role="alert" key={result.className}>
+              {results.length > 0 &&
+                results.map((result, index) => (
+                  <div
+                    className="alert alert-success"
+                    role="alert"
+                    key={result.className}
+                  >
                     <h4 className="alert-heading">{result.className}</h4>
                     <p>
-                      Confidence level: <span className="font-weight-bold">{(result.probability * 100).toFixed(2)}%</span>
-                      {index === 0 && <span className="text-muted small ml-2">(Best Guess)</span>}
+                      Confidence level:{" "}
+                      <span className="font-weight-bold">
+                        {(result.probability * 100).toFixed(2)}%
+                      </span>
+                      {index === 0 && (
+                        <span className="text-muted small ml-2">
+                          (Best Guess)
+                        </span>
+                      )}
                     </p>
                   </div>
-                );
-              })}
-            </div>
-          )}
-          {imageURL && (
-            <button className="btn btn-success" onClick={identify}>
-              Identify Image
-            </button>
-          )}
-        </div>
-        {history.length > 0 && (
-          <div className="col-md-6">
-            <h2 className='m-2 '>Recent Images</h2>
-            <div className="row">
-              {history.map((image, index) => {
-                return (
-                  <div className="col-4 mb-4" key={`${image}${index}`}>
-                    <img src={image} alt="Recent Prediction" className="img-fluid rounded shadow" onClick={() => setImageURL(image)} />
-                  </div>
-                );
-              })}
+                ))}
             </div>
           </div>
-        )}
+          <div className="text-center mt-4">
+            <h2 className="mb-3">Search History</h2>
+            <div className="row justify-content-center">
+              {history.map((image, index) => (
+                <div className="col-6 col-md-4 mb-4" key={`${image}${index}`}>
+                  <img
+                    src={image}
+                    alt="Recent Prediction"
+                    className="img-fluid rounded shadow"
+                    onClick={() => setImageURL(image)}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-
