087ecbea834990efe51f0b81d6ca531c13782d70
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-05-19 17:37:55 -0400

    Styling: added react-boostrap styles to form

--- pages/login.js
+++ pages/login.js
@@ -2,8 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import Link from "next/link";
-
-// TODO: Add bootstrap styling and implement a DEMO login button
+import { Form, Button, Alert, Container, Row, Col } from "react-bootstrap";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -16,9 +15,9 @@ export default function Login() {
 
     try {
       const response = await axios.post("/api/login", { email, password });
-      if (response.status == 200) {
-        localStorage.setItem("token", response.data.token); // Store the token in local storage
-        router.push("/"); // Redirect to homepage on successful login
+      if (response.status === 200) {
+        localStorage.setItem("token", response.data.token);
+        router.push("/");
       } else {
         setError(response.data.message);
       }
@@ -27,30 +26,78 @@ export default function Login() {
     }
   };
 
+  const handleDemoLogin = async () => {
+    // Example demo login logic
+    const demoEmail = "demo@example.com";
+    const demoPassword = "demopassword";
+
+    setEmail(demoEmail);
+    setPassword(demoPassword);
+
+    // Simulate form submission
+    handleLogin(new Event("submit"));
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          required
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
-      {error && <div>{error}</div>}
-      <div>
-        Don't have an account? <Link href="/register">Register</Link>
-      </div>
-    </div>
+    <Container className="mt-5">
+      <Row className="justify-content-center">
+        <Col xs={12} md={6}>
+          <div className="text-center mb-4">
+            <img
+              src="/ImageSense-logos/ImageSense-logos_black.png"
+              alt="ImageSense Logo"
+              className="img-fluid"
+            />
+          </div>
+        </Col>
+        <Col xs={12} md={6} className="d-flex align-items-center">
+          <div className="w-100 m-4">
+            <h1 className="text-center mb-4">Login</h1>
+            <Form onSubmit={handleLogin}>
+              <Form.Group controlId="formEmail">
+                <Form.Label className="text-center m-2">Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your email"
+                  required
+                  aria-label="Email"
+                />
+              </Form.Group>
+              <Form.Group controlId="formPassword">
+                <Form.Label className="text-center m-2">Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Enter your password"
+                  required
+                  aria-label="Password"
+                />
+              </Form.Group>
+              <div className="text-center mt-4">
+                <Button variant="primary" type="submit" className="m-2">
+                  Login
+                </Button>
+                <Button
+                  variant="secondary"
+                  onClick={handleDemoLogin}
+                  className="m-2"
+                >
+                  Demo Login
+                </Button>
+              </div>
+            </Form>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <div className="text-center mt-3">
+              <p>
+                Don't have an account? <Link href="/register">Register</Link>
+              </p>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
